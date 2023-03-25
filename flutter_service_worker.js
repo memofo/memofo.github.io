6d6238e3ebf7b0d2a8e495375db2e69ce2f8b34d
Eduardo <egsantos128@gmail.com>
2023-03-25 19:38:26 -0300

    implement sign-in silently

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5d9cdd0af403a47a586cb35de9be10bb",
-"/": "5d9cdd0af403a47a586cb35de9be10bb",
-"main.dart.js": "77467ea1aafa0c15a33f8a9d79e1260d",
+"index.html": "c3155494963e81819a0b64543efcc875",
+"/": "c3155494963e81819a0b64543efcc875",
+"main.dart.js": "22ea4dab8019ff05324c0989b5e3310e",
 "manifest.json": "c7febb22b06d1c576e6b61ba749c8d35",
 "version.json": "c0446c417f16c922e4d5a24dce638fcf"
 };
