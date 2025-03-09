7bd66b062c6b289035dd69e1d63c3f304c315116
Eduardo <egsantos128@gmail.com>
2025-03-09 17:05:47 -0300

    attempt 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1882348984"
+    serviceWorkerVersion: "656525882"
   }
 });
