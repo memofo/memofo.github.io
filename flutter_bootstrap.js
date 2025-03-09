9e9a8d56e828876204f8d7c4a860a9f2b666eb8b
Eduardo Gomes <egsantos128@gmail.com>
2025-03-09 19:04:14 +0000

    add firebase authentication and dual write to db

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "312887744"
+    serviceWorkerVersion: "1285056610"
   }
 });
